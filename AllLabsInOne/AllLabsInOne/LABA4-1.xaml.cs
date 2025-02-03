2f9fa0ef107ad58e09dfce12093bed44ca5c2687
druap <76074150+d404mer@users.noreply.github.com>
2025-02-03 21:03:45 +0300

    блять да в пизду это всё

---
+++ AllLabsInOne/AllLabsInOne/LABA4-1.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AllLabsInOne
+{
+    /// <summary>
+    /// Interaction logic for LABA4_1.xaml
+    /// </summary>
+    public partial class LABA4_1 : Window
+    {
+        public LABA4_1()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Danilova A.A. 4237");
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Danilova A.A. 4237");
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Background = System.Windows.Media.Brushes.Red;
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            this.Background = System.Windows.Media.Brushes.Yellow;
+        }
+    }
+}
---
+++ LB5/LABA5_1/LABA5_1/MainWindow.axaml.cs
@@ -0,0 +1,53 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+using MsBox.Avalonia;
+using System;
+
+namespace LABA5_1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.AttachDevTools();
+        }
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        // Обработчик для кнопки "Exit"
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        // Обработчик для кнопки "About"
+        private void AboutButton_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            var messageBox = new MessageBoxWindow();
+            messageBox.WindowStartupLocation = Avalonia.Controls.WindowStartupLocation.CenterScreen;
+            messageBox.Width = 200;
+            messageBox.Height = 100;
+            messageBox.ShowMessage("Данилова 4337");
+            messageBox.ShowDialog(this);  
+        }
+
+        // Обработчик для кнопки "Red"
+        private void RedButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Background = Avalonia.Media.Brushes.Red;
+        }
+
+        // Обработчик для кнопки "Yellow"
+        private void YellowButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Background = Avalonia.Media.Brushes.Yellow;
+        }
+
+    }
+}
