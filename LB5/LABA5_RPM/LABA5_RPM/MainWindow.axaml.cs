fa20063e370f46941e82689b5762c624377b1330
druap <76074150+d404mer@users.noreply.github.com>
2025-02-03 19:36:12 +0300

    LB5 part 1

---
+++ LB5/LABA5_RPM/LABA5_RPM/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace LABA5_RPM
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
