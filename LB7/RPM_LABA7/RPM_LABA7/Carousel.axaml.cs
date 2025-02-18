1ed7aadab1dade29abd56150cf0d02fe1f977107
d0h <dev1lc3st@gmail.com>
2025-02-18 22:59:45 +0300

    карусель ну почти

--- LB7/RPM_LABA7/RPM_LABA7/Carousel.axaml.cs
+++ LB7/RPM_LABA7/RPM_LABA7/Carousel.axaml.cs
@@ -1,34 +1,154 @@
 using Avalonia;
-using Avalonia.Animation.Easings;
 using Avalonia.Animation;
 using Avalonia.Controls;
 using Avalonia.Input;
-using Avalonia.Markup.Xaml;
+using Avalonia.Media;
 using Avalonia.Styling;
 using System;
-using Avalonia.Interactivity;
+using System.Threading;
+using System.Threading.Tasks;
 
-namespace RPM_LABA7;
-
-public partial class Carousel : Window
+namespace RPM_LABA7
 {
-    public Carousel()
+    public partial class Carousel : Window
     {
-        InitializeComponent();
-        // Подписываемся на нажатие
-        img1.PointerPressed += OnImageTapped;
-        img2.PointerPressed += OnImageTapped;
-    }
-    private async void OnImageTapped(object? sender, PointerPressedEventArgs e)
-    {
-    
-        if (sender is Image image)
+        private ScaleTransform scaleTransform;
+        private SkewTransform skewTransform;
+
+        // Объявляем анимируемые свойства
+        public static readonly StyledProperty<double> ImageScaleXProperty =
+            AvaloniaProperty.Register<Carousel, double>(nameof(ImageScaleX));
+        public static readonly StyledProperty<double> ImageScaleYProperty =
+            AvaloniaProperty.Register<Carousel, double>(nameof(ImageScaleY));
+        public static readonly StyledProperty<double> ImageSkewAngleYProperty =
+            AvaloniaProperty.Register<Carousel, double>(nameof(ImageSkewAngleY));
+
+        public double ImageScaleX
         {
-            // Запускаем анимацию из ресурсов
-            if (this.Resources.TryGetResource("ImageAnimation", null, out var anim) && anim is Animation animation)
+            get => GetValue(ImageScaleXProperty);
+            set
             {
-                await animation.RunAsync(image);
+                SetValue(ImageScaleXProperty, value);
+                if (scaleTransform != null)
+                    scaleTransform.ScaleX = value;
             }
+        }
+
+        public double ImageScaleY
+        {
+            get => GetValue(ImageScaleYProperty);
+            set
+            {
+                SetValue(ImageScaleYProperty, value);
+                if (scaleTransform != null)
+                    scaleTransform.ScaleY = value;
+            }
+        }
+
+        public double ImageSkewAngleY
+        {
+            get => GetValue(ImageSkewAngleYProperty);
+            set
+            {
+                SetValue(ImageSkewAngleYProperty, value);
+                if (skewTransform != null)
+                    skewTransform.AngleY = value;
+            }
+        }
+
+        public Carousel()
+        {
+            InitializeComponent();
+            var image = this.FindControl<Image>("image");
+
+            // Получаем ScaleTransform и SkewTransform из RenderTransform
+            if (image.RenderTransform is TransformGroup transformGroup)
+            {
+                scaleTransform = transformGroup.Children[0] as ScaleTransform;
+                skewTransform = transformGroup.Children[1] as SkewTransform;
+            }
+
+            // Инициализируем свойства начальными значениями
+            ImageScaleX = 1.0;
+            ImageScaleY = 1.0;
+            ImageSkewAngleY = 22.0;
+
+            // Подписываемся на событие PointerPressed
+            image.PointerPressed += Image_PointerPressed;
+        }
+
+        private async void Image_PointerPressed(object? sender, PointerPressedEventArgs e)
+        {
+            if (scaleTransform == null || skewTransform == null)
+            {
+                Console.WriteLine("ScaleTransform или SkewTransform не найдены!");
+                return;
+            }
+
+            // Анимация для ImageScaleX
+            var scaleXAnimation = new Animation
+            {
+                Duration = TimeSpan.FromSeconds(1),
+                FillMode = FillMode.Forward,
+                Children =
+                {
+                    new KeyFrame
+                    {
+                        Cue = new Cue(0),
+                        Setters = { new Setter(ImageScaleXProperty, 1.0) }
+                    },
+                    new KeyFrame
+                    {
+                        Cue = new Cue(1),
+                        Setters = { new Setter(ImageScaleXProperty, 2.25) }
+                    }
+                }
+            };
+
+            // Анимация для ImageScaleY
+            var scaleYAnimation = new Animation
+            {
+                Duration = TimeSpan.FromSeconds(1),
+                FillMode = FillMode.Forward,
+                Children =
+                {
+                    new KeyFrame
+                    {
+                        Cue = new Cue(0),
+                        Setters = { new Setter(ImageScaleYProperty, 1.0) }
+                    },
+                    new KeyFrame
+                    {
+                        Cue = new Cue(1),
+                        Setters = { new Setter(ImageScaleYProperty, 2.25) }
+                    }
+                }
+            };
+
+            // Анимация для ImageSkewAngleY
+            var skewAnimation = new Animation
+            {
+                Duration = TimeSpan.FromSeconds(1),
+                FillMode = FillMode.Forward,
+                Children =
+                {
+                    new KeyFrame
+                    {
+                        Cue = new Cue(0),
+                        Setters = { new Setter(ImageSkewAngleYProperty, 22.0) }
+                    },
+                    new KeyFrame
+                    {
+                        Cue = new Cue(1),
+                        Setters = { new Setter(ImageSkewAngleYProperty, 0.0) }
+                    }
+                }
+            };
+
+            // Запускаем анимации на this (окно наследует Visual)
+            await scaleXAnimation.RunAsync(this, CancellationToken.None);
+            await scaleYAnimation.RunAsync(this, CancellationToken.None);
+            await skewAnimation.RunAsync(this, CancellationToken.None);
         }
     }
 }
--- LB7/RPM_LABA7/RPM_LABA7/obj/Debug/net8.0/RPM_LABA7.AssemblyInfo.cs
+++ LB7/RPM_LABA7/RPM_LABA7/obj/Debug/net8.0/RPM_LABA7.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RPM_LABA7")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b678017e534f336afe6967842ce90e0b78c75d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+764f1c71a0ce7c844e51a8049411968aea77158c")]
 [assembly: System.Reflection.AssemblyProductAttribute("RPM_LABA7")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RPM_LABA7")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
