5520d8df1e5fe43127a8ac7bcd227be9f1953d8d
d0h <dev1lc3st@gmail.com>
2025-03-21 11:42:17 +0300

    minor changes

--- LB6/open_lock_2textbox/TextNotEmptyToIsEnabledConverter.cs
+++ LB6/open_lock_2textbox/TextNotEmptyToIsEnabledConverter.cs
@@ -6,7 +6,7 @@ public class TextNotEmptyToIsEnabledConverter : IValueConverter
 {
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
     {
-        return !string.IsNullOrWhiteSpace(value as string); // Если есть текст поле доступно для редактирования
+        return !string.IsNullOrWhiteSpace(value as string);
     }
 
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
